fa3b3f3e785783d5a51dc813f8e711e324909ac1
Adesh Choudhar <choudharas19.comp@coep.ac.in>
2022-06-26 17:42:00 +0530

    Initial commit

---
+++ src/main/java/com/example/springshapes/SpringShapesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springshapes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShapesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShapesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springshapes/SpringShapesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springshapes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShapesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
